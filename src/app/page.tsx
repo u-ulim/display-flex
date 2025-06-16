615137dd1b2d12a37d02b4eab2dfbce0690985e5
u-ulim <uiissoop@gmail.com>
2025-06-16 13:51:12 +0900

    refactor: 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { BannerSlide } from "./components/slide/banner";
-import { PopularSlide } from "./components/slide/popular";
-import { NewSlide } from "./components/slide/new";
-
 export default function App() {
-  return (
-    <main>
-      <BannerSlide />
-      <PopularSlide />
-      <NewSlide />
-    </main>
-  );
+  return <main>hi</main>;
 }
